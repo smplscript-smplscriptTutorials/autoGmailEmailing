e13731e930e233369a51093f45f484a340578b22
smplscript-smplscriptTutorials <108156041+smplscript-smplscriptTutorials@users.noreply.github.com>
2023-01-24 17:04:31 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import automatedEmailing.automatedEmailing
+
+# Use email function
+
+automatedEmailing.automatedEmailing.email("""sender@gmail.com""", """password""", """receiver""", "Test subject", """Body""")
