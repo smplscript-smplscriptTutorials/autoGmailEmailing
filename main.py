2c32e876e1fa843eff3d172a1ab78948a5b912f7
smplscript-smplscriptTutorials <108156041+smplscript-smplscriptTutorials@users.noreply.github.com>
2023-01-24 17:08:40 -0500

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import automatedEmailing.automatedEmailing
-
-# Use email function
-
-automatedEmailing.automatedEmailing.email("""sender@gmail.com""", """password""", """receiver""", "Test subject", """Body""")
